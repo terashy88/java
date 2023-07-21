8339ad7a16814336cc6baa48fa0faf0f179b2e85
terashy <terashy@bluewin.ch>
2023-07-21 14:22:06 +0200

    .

---
+++ rsx/src/main/java/com/terashy/rsx/RsxApplication.java
@@ -0,0 +1,13 @@
+package com.terashy.rsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsxApplication.class, args);
+	}
+
+}
---
+++ rsx/src/main/java/com/terashy/rsx/archiv/Scanner.java
@@ -0,0 +1,16 @@
+package com.terashy.rsx.archiv;
+
+/**
+ * test
+ */
+public class Scanner {
+
+    public static void main(String[] args) {
+
+        // String input with the Java Scanner
+        System.out.println("Your Input: ");
+        Scanner i = new Scanner(System.in);
+        String o = i.next();
+        System.out.println(o);
+    }
+}
